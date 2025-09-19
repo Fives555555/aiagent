b4c81d89316b43611bceeca65dde1ce77dd3907b
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-09-19 17:45:10 +0100

    Clean up code

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
+import os
+import sys
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
 
 def main():
-    
-    import os
-    import sys
-    from dotenv import load_dotenv
-    from google import genai
-    from google.genai import types
-
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    client = genai.Client(api_key=api_key)
-
-    try:
-        user_prompt = sys.argv[1]
-    except Exception:
-        print("No prompt provided")
+    
+    args = sys.argv[1:]
+    
+    if not args:
+        print("AI Code Assistant\n")
+        print('Usage: python main.py "user prompt"\n')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    user_prompt = " ".join(args)
 
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
     )
-    
-    print(response.text)
-    
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
