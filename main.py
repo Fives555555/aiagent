d622365af46c87c50327330543f9d774be1be578
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-09-19 17:22:28 +0100

    Add tracking ability for conversations

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     import sys
     from dotenv import load_dotenv
     from google import genai
+    from google.genai import types
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,15 +13,22 @@ def main():
     client = genai.Client(api_key=api_key)
 
     try:
-        content = sys.argv[1]
+        user_prompt = sys.argv[1]
     except Exception:
         print("No prompt provided")
         sys.exit(1)
 
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=content
+        model='gemini-2.0-flash-001', 
+        contents=messages
     )
+    
     print(response.text)
+    
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
