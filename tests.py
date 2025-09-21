f015ea9b1bbeaa49d3c8c2ec396e51163779a878
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-09-21 09:55:49 +0100

    Update tests for getting file content

--- tests.py
+++ tests.py
@@ -1,26 +1,30 @@
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def test():
-    result1 = get_files_info("calculator", ".")
+    # result1 = get_files_info("calculator", ".")
 
-    print(f"Result for current directory:")
-    print(f"{result1}")
+    # print(f"Result for current directory:")
+    # print(f"{result1}")
 
-    result2 = get_files_info("calculator", "pkg")
+    # result2 = get_files_info("calculator", "pkg")
 
-    print(f"Result for 'pkg' directory:")
-    print(f"{result2}")
+    # print(f"Result for 'pkg' directory:")
+    # print(f"{result2}")
 
-    result3 = get_files_info("calculator", "/bin")
+    # result3 = get_files_info("calculator", "/bin")
 
-    print(f"Result for '/bin' directory:")
-    print(f"{result3}")
+    # print(f"Result for '/bin' directory:")
+    # print(f"{result3}")
 
-    result4 = get_files_info("calculator", "../")
+    # result4 = get_files_info("calculator", "../")
 
-    print(f"Result for '../' directory:")
-    print(f"{result4}")
-
+    # print(f"Result for '../' directory:")
+    # print(f"{result4}")
+    
+    result = get_file_content("calculator", "lorem.txt")
+    print(f"Result for lorem:")
+    print(result)
 
 if __name__ == "__main__":
     test()
