459a297ee1054dfca7798b6e1483751c58ff579c
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-09-21 09:55:23 +0100

    Function to get the contents of a file

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+from .config import *
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+
+    try:
+        target = os.path.realpath(full_path)
+        root = os.path.realpath(working_directory)
+        
+        if os.path.commonpath([target, root]) != root:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(target, "r") as f:
+            file_content_string = f.read()
+            if len(file_content_string) > MAX_CHARS:
+                return file_content_string[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+            return file_content_string
+        
+    except Exception as e:
+        return f'Error: {e}'
+        
+    
