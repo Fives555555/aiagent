d2ed739a4a933ef1baaaacec47719821f849570c
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-09-21 09:59:02 +0100

    Update tests

--- tests.py
+++ tests.py
@@ -22,8 +22,20 @@ def test():
     # print(f"Result for '../' directory:")
     # print(f"{result4}")
     
-    result = get_file_content("calculator", "lorem.txt")
-    print(f"Result for lorem:")
+    # result = get_file_content("calculator", "lorem.txt")
+    # print(f"Result for lorem:")
+    # print(result)
+    
+    result = get_file_content("calculator", "main.py")
+    print(result)
+    
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+    
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+    
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
     print(result)
 
 if __name__ == "__main__":
